06525080710ba848e438dcb26b0c07a423e848a8
Dannys02 <webdannys@gmail.com>
2026-03-13 21:45:12 +0700

    ubah http -> https tailwind cek status

--- resources/views/booking/cek-status.blade.php
+++ resources/views/booking/cek-status.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <script src="http://cdn.tailwindcss.com"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
   <title>Cek Status</title>
 </head>
 
